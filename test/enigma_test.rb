a1dcd967d403dc1c7168f2a869ceeeb657a8a4da
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-18 15:07:16 -0500

    Add basic test for Enigma class

---
+++ test/enigma_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+end
