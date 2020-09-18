40aff8c89c01ee4b7178236b2be4b586e7c147d9
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-18 18:27:42 -0500

    Add todays_date method to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,9 @@
 class Enigma
   def generate_key
-    5.times.map { rand(0..9) }.join.to_i
+    5.times.map { rand(0..9) }.join
+  end
+
+  def todays_date
+    Time.now.strftime("%d/%m/%y").delete('/')
   end
 end
