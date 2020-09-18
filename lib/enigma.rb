d6c457e2503e2501ef8a02030a4e1b8ae9c8ae28
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-18 18:54:52 -0500

    Add generate_offset method to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,22 @@
 class Enigma
+  attr_reader :character_set 
+
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def encrypt(message, key = generate_key, date = todays_date)
+
+  end
+
   def generate_key
     5.times.map { rand(0..9) }.join
+  end
+
+  def generate_offset(date)
+    offset = date.to_i ** 2
+
+    offset.to_s.chars.last(4).join
   end
 
   def todays_date
