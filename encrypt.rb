ec8a0824c3a49c2743e05c3d90583940a8adfaa7
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-19 12:51:27 -0500

    Create encrypt runner file

---
+++ encrypt.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/enigma'
+require_relative '../lib/writer'
+require_relative '../lib/messenger'
+
+
+enigma = Enigma.new
+writer = Writer.new
+    
+messenger = Messenger.new(enigma, writer)
+
