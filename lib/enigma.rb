d4582b54486dfa4f16643f832edba0c3a9a645f8
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-18 19:16:58 -0500

    Add methods for finding shift amounts to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,8 @@
 class Enigma
-  attr_reader :character_set 
+  attr_reader :character_set
 
   def initialize
-    @character_set = ("a".."z").to_a << " "
+    @character_set = ('a'..'z').to_a << ' '
   end
 
   def encrypt(message, key = generate_key, date = todays_date)
@@ -14,12 +14,28 @@ class Enigma
   end
 
   def generate_offset(date)
-    offset = date.to_i ** 2
+    offset = date.to_i**2
 
     offset.to_s.chars.last(4).join
   end
 
   def todays_date
-    Time.now.strftime("%d/%m/%y").delete('/')
+    Time.now.strftime('%d/%m/%y').delete('/')
+  end
+
+  def a_shift(key, offset)
+    key[0..1].to_i + offset[0].to_i
+  end
+
+  def b_shift(key, offset)
+    key[1..2].to_i + offset[1].to_i
+  end
+
+  def c_shift(key, offset)
+    key[2..3].to_i + offset[2].to_i
+  end
+
+  def d_shift(key, offset)
+    key[3..4].to_i + offset[3].to_i
   end
 end
