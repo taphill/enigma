bf45edbbf829c5865519de114d3875302630df71
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-19 13:00:30 -0500

    Add test for attributes to Messenger class

--- test/messenger_test.rb
+++ test/messenger_test.rb
@@ -11,4 +11,9 @@ class MessengerTest < Minitest::Test
   def test_it_exists
     assert_instance_of Messenger, @messenger
   end
+
+  def test_it_has_attributes
+    assert_instance_of Enigma, @messenger.enigma
+    assert_instance_of Writer, @messenger.writer
+  end
 end
